81949e55b791a6bbb9737ac475d914b6f362c737
System Administrator <root@philbook-2.local>
2020-09-14 12:40:33 +0200

    production build

---
+++ dist/precache-manifest.fb2e8a76c7d05951ae0da718f04f3d7d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fa9f469d50e494e5bbc",
+    "url": "/css/app.4cfc6ce0.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "8bff91e69fd912efcc3ba10ab340088c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a03d2b00d583f6a60eb",
+    "url": "/js/about.c787ca3d.js"
+  },
+  {
+    "revision": "0fa9f469d50e494e5bbc",
+    "url": "/js/app.c6db264a.js"
+  },
+  {
+    "revision": "8f2693a9809740ca8ac1",
+    "url": "/js/chunk-vendors.16b0917d.js"
+  },
+  {
+    "revision": "b86ee9d79e84e8a95bad050900aaec1e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
