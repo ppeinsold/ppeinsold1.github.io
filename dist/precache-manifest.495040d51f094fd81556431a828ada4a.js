f1a206d73435b5cf1ad3925312a9ce85ce93fd33
ppeinsold <philip@herosphere.gg>
2020-09-19 22:25:02 +0200

    Implemented basic structure

---
+++ dist/precache-manifest.495040d51f094fd81556431a828ada4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8aa1d96086fef6e6b28",
+    "url": "/css/app.f85aa9e1.css"
+  },
+  {
+    "revision": "0386678a1388efc248a7197de66e5904",
+    "url": "/img/profile.0386678a.jpg"
+  },
+  {
+    "revision": "1479a208ae5a7b959747b6d649cc0f55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8aa1d96086fef6e6b28",
+    "url": "/js/app.b6da663e.js"
+  },
+  {
+    "revision": "962fe72f3dd11fb34465",
+    "url": "/js/chunk-vendors.76da3347.js"
+  },
+  {
+    "revision": "b86ee9d79e84e8a95bad050900aaec1e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
